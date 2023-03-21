1f5f6afe32069f6438f5e7bff8b481fe5264181d
goodness <goodyc474@gmail.com>
2023-03-21 21:10:42 +0100

    add javascript function

---
+++ app.js
@@ -0,0 +1,7 @@
+const day = document.querySelector(".day");
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+const currentTime = new Date();
+console.log(currentTime);
